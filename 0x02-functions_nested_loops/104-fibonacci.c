9c0b0a9b9b9f69abfcb928e6b4c314ba747724b9
douaa <douaasaddiqe02@gmail.com>
2023-03-21 11:03:50 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,22 +6,23 @@
  */
 int main(void)
 {
-int i;
-unsigned long int f1 = 1;
-unsigned long int f2 = 2;
-unsigned long int n;
-printf("%lu, ", f1);
-printf("%lu, ", f2);
-for (i = 3; i < 98; i++)
-{
-	n = f2 + f1;
-	printf("%lu, ", n);
-	f1 = f2;
-	f2 = n;
-}
+        int i;
+        unsigned long int f1 = 1;
+        unsigned long int f2 = 2;
+        unsigned long int n;
+        printf("%lu, ", f1);
+        printf("%lu, ", f2);
+        for (i = 3; i < 98; i++){
+		h
+        	n = f2 + f1;
+        	printf("%lu, ", n);
+	        f1 = f2;
+        	f2 = n;
+	}
+
 n = f2 + f1;
 printf("%lu\n", n);
 
-
 return (0);
 }
+

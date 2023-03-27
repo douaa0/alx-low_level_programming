847f9290093f037219090f223785f4915f0e77fb
douaa <douaasaddiqe02@gmail.com>
2023-03-27 03:55:00 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * print_array - a function that prints n elements of an array
@@ -20,3 +20,4 @@ void print_array(int *a, int n)
 		}
 			printf("\n");
 }
+

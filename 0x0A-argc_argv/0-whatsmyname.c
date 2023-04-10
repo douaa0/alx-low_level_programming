8fb328f48f2abf51536cef3a24cb4930ad4e672a
douaa <douaasaddiqe02@gmail.com>
2023-04-10 13:58:28 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int a = argc;
+a = 0;
+puts(argv[a]);
+return (0);
+}

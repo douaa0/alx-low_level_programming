1d0a0e588a232beccbeacbf129afe270c98962a0
douaa <douaasaddiqe02@gmail.com>
2023-04-10 14:31:36 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * Return: Always 0 which means success
+ */
+int main(int argc, char *argv[])
+{
+int i,  a = argc;
+char **b = argv;
+if (b != NULL)
+	for (i = 0; i < b; i++)
+	{
+		printf("%s\n", b[i]);
+	}
+return (0);
+}

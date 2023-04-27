f156a13586aad2ca44d2b4ef46e125d63d065ae7
douaa0 <126075553+douaa0@users.noreply.github.com>
2023-04-27 19:37:55 +0000

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked list_t list
- * @h: pointer to the list_t list
- * Return: number of elements in h
+ * list_len - determines length 
+ * of linked list given
+ * @h: pointer to first node
+ * Return: size of list
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t j = 0;
 
 	while (h)
 	{
-		n++;
 		h = h->next;
+		j++;
 	}
-	return (n);
+	return (j);
 }

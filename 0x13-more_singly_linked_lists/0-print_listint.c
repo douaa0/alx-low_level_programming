ae5078f08bb1d7ad795f8757c9dfd42140ef00d7
douaa0 <126075553+douaa0@users.noreply.github.com>
2023-05-01 22:41:22 +0100

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint -  a function that prints
+ * all the elements of a listint_t list
+ * @h: pointer to first node
+ * Return: size of list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		j++;
+	}
+	return (j);
+}

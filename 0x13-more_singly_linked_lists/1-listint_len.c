7690a20689940ca7d6946cb3dff13f72126366ce
douaa0 <126075553+douaa0@users.noreply.github.com>
2023-05-01 22:43:05 +0100

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len -  a function that returns the number
+ * of elements in a linked listint_t list
+ * @h: pointer to first node
+ * Return: size of list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		h = h->next;
+		j++;
+	}
+	return (j);
+}

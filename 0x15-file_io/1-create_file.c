456d36d1afb9225786089f1f389650933c94d19d
douaa0 <126075553+douaa0@users.noreply.github.com>
2023-05-08 22:03:25 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int s, w, len = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	s = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (s == -1 || w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	close(s);
+	close(fd);
 
 	return (1);
 }
